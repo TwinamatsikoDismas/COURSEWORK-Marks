91c65d3ca7c83f3770b69c3eb1b41b0fa203e291
Image <dismast32@gmail.com>
2024-09-30 14:00:06 +0300

    Add files via upload

---
+++ Coursework.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Coursework
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        LblGRADINGSCALECALCULATOR = New Label()
+        LblEnterMarks = New Label()
+        LblGrade = New Label()
+        btnEvaluate = New Button()
+        txtEnterMarks = New TextBox()
+        SuspendLayout()
+        ' 
+        ' LblGRADINGSCALECALCULATOR
+        ' 
+        LblGRADINGSCALECALCULATOR.AutoSize = True
+        LblGRADINGSCALECALCULATOR.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LblGRADINGSCALECALCULATOR.Location = New Point(217, 65)
+        LblGRADINGSCALECALCULATOR.Name = "LblGRADINGSCALECALCULATOR"
+        LblGRADINGSCALECALCULATOR.Size = New Size(301, 30)
+        LblGRADINGSCALECALCULATOR.TabIndex = 0
+        LblGRADINGSCALECALCULATOR.Text = "GRADING SCALE CALCULATOR"
+        ' 
+        ' LblEnterMarks
+        ' 
+        LblEnterMarks.AutoSize = True
+        LblEnterMarks.Location = New Point(199, 143)
+        LblEnterMarks.Name = "LblEnterMarks"
+        LblEnterMarks.Size = New Size(69, 15)
+        LblEnterMarks.TabIndex = 1
+        LblEnterMarks.Text = "Enter Marks"
+        ' 
+        ' LblGrade
+        ' 
+        LblGrade.AutoSize = True
+        LblGrade.Location = New Point(372, 197)
+        LblGrade.Name = "LblGrade"
+        LblGrade.Size = New Size(38, 15)
+        LblGrade.TabIndex = 2
+        LblGrade.Text = "Grade"
+        ' 
+        ' btnEvaluate
+        ' 
+        btnEvaluate.Location = New Point(324, 238)
+        btnEvaluate.Name = "btnEvaluate"
+        btnEvaluate.Size = New Size(141, 23)
+        btnEvaluate.TabIndex = 3
+        btnEvaluate.Text = "Evaluate"
+        btnEvaluate.UseVisualStyleBackColor = True
+        ' 
+        ' txtEnterMarks
+        ' 
+        txtEnterMarks.Location = New Point(292, 140)
+        txtEnterMarks.Name = "txtEnterMarks"
+        txtEnterMarks.Size = New Size(226, 23)
+        txtEnterMarks.TabIndex = 4
+        ' 
+        ' Coursework
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtEnterMarks)
+        Controls.Add(btnEvaluate)
+        Controls.Add(LblGrade)
+        Controls.Add(LblEnterMarks)
+        Controls.Add(LblGRADINGSCALECALCULATOR)
+        Name = "Coursework"
+        Text = "GRADING CALCULATOR"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LblGRADINGSCALECALCULATOR As Label
+    Friend WithEvents LblEnterMarks As Label
+    Friend WithEvents LblGrade As Label
+    Friend WithEvents btnEvaluate As Button
+    Friend WithEvents txtEnterMarks As TextBox
+
+End Class
---
+++ Coursework.vb
@@ -0,0 +1,51 @@
+﻿Public Class Coursework
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtGrade_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnEvaluate_Click(sender As Object, e As EventArgs) Handles btnEvaluate.Click
+        Dim Marks As Integer
+        'input validation for marks to ensure numeric input'
+        If Not Integer.TryParse(txtEnterMarks.Text, Marks) Then
+            MessageBox.Show("please enter a valid numeric value for marks.")
+            Exit Sub
+        End If
+        'check if marks exceeds 100
+        If Marks > 100 Then
+            MessageBox.Show("wrong entry,please re-enter the marks")
+            Exit Sub
+        End If
+        'using select case to determine the grade best on marks
+        Select Case Marks
+            Case 90 To 100
+                LblGrade.Text = "Excellent"
+            Case 80 To 89
+                LblGrade.Text = "Very Good"
+            Case 70 To 79
+                LblGrade.Text = "Good"
+            Case 60 To 69
+                LblGrade.Text = "Medium"
+            Case 50 To 59
+                LblGrade.Text = "Pass"
+            Case 0 To 49
+                LblGrade.Text = "Fail"
+            Case Else
+                LblGrade.Text = "Invalid Marks"
+        End Select
+    End Sub
+
+    Private Sub txtEnterMarks_TextChanged(sender As Object, e As EventArgs) Handles txtEnterMarks.TextChanged
+
+    End Sub
+
+    Private Sub LblGrade_Click(sender As Object, e As EventArgs) Handles LblGrade.Click
+    End Sub
+
+    Private Sub LblEnterMarks_Click(sender As Object, e As EventArgs) Handles LblEnterMarks.Click
+
+    End Sub
+End Class
